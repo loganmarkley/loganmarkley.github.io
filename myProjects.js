5524473f45aa88f4c8e5e007610810bb8e985f67
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-08-02 12:52:47 -0500

    Added Language Images to MyProjects!
    also added some css

--- myProjects.js
+++ myProjects.js
@@ -1,5 +1,6 @@
 //console.log(concatenateString('gif',3));
 
+highlightLanguages(1);
 let currentProjectNumber = 1;
 let numOfProjects = 3;
 let titleID = concatenateString('title',currentProjectNumber);
@@ -21,7 +22,7 @@ function rotateLeft()
 
     document.getElementById(titleID).style.display = 'block';
     document.getElementById(gifID).style.display = 'block';
-
+    highlightLanguages(currentProjectNumber);
 }
 
 function rotateRight()
@@ -40,6 +41,7 @@ function rotateRight()
 
     document.getElementById(titleID).style.display = 'block';
     document.getElementById(gifID).style.display = 'block';
+    highlightLanguages(currentProjectNumber);
 }
 
 function concatenateString(strg, idNUM)
@@ -48,4 +50,56 @@ function concatenateString(strg, idNUM)
         return 'proj' + idNUM + 'Title';
     if(strg == 'gif')
         return 'proj' + idNUM + 'GIF';
+}
+
+function highlightLanguages(idNUM)
+{
+    darkenAllLanguages();
+    if(idNUM == 1)
+    {
+        document.getElementById('python').style.opacity = '100%';
+        document.getElementById('python').style.borderStyle = 'solid';
+        document.getElementById('python').style.borderWidth = '1px';
+    }
+    else if(idNUM == 2)
+    {
+        document.getElementById('java').style.opacity = '100%';
+        document.getElementById('java').style.borderStyle = 'solid';
+        document.getElementById('java').style.borderWidth = '1px';
+    }
+    else if(idNUM == 3)
+    {
+        document.getElementById('javascript').style.opacity = '100%';
+        document.getElementById('javascript').style.borderStyle = 'solid';
+        document.getElementById('javascript').style.borderWidth = '1px';
+
+        document.getElementById('html').style.opacity = '100%';
+        document.getElementById('html').style.borderStyle = 'solid';
+        document.getElementById('html').style.borderWidth = '1px';
+
+        document.getElementById('css').style.opacity = '100%';
+        document.getElementById('css').style.borderStyle = 'solid';
+        document.getElementById('css').style.borderWidth = '1px';
+    }
+}
+
+function darkenAllLanguages()
+{
+    document.getElementById('cplusplus').style.opacity = '30%';
+    document.getElementById('cplusplus').style.borderWidth = '0px';
+    
+    document.getElementById('python').style.opacity = '30%';
+    document.getElementById('python').style.borderWidth = '0px';
+    
+    document.getElementById('java').style.opacity = '30%';
+    document.getElementById('java').style.borderWidth = '0px';
+    
+    document.getElementById('javascript').style.opacity = '30%';
+    document.getElementById('javascript').style.borderWidth = '0px';
+    
+    document.getElementById('html').style.opacity = '30%';
+    document.getElementById('html').style.borderWidth = '0px';
+    
+    document.getElementById('css').style.opacity = '30%';
+    document.getElementById('css').style.borderWidth = '0px';
 }
